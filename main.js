794695a65591c9de22e60cabc154a6924b3b9d30
sebro-mustafa <sebreen.khatib@hotmail.com>
2025-03-05 01:21:15 +0200

    first commit

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ document.getElementById('btn-add').addEventListener('click',()=>{
     if(note){
         let note_item=document.createElement('li')
         note_item.classList.add('py-3','px-5','d-flex','justify-content-between','fs-4','list-item')
-        note_item.innerHTML=`<p style="color:#9C69B6;">${note}</p>
+        note_item.innerHTML=`<p class="note-of-todo" style="color:#9C69B6;">${note}</p>
         <div class="d-flex ">
-        <input type="checkbox" id="check" class="ms-4 form-check-input my-2" >
-        <button id='btn_remove' class="btn_remove btn  ms-2 p-2 text-white fs-4" style="background-color:#A30132; ">Delete</button>
+        <input type="checkbox"  class="check ms-4 form-check-input my-2" >
+        <button  class="btn_remove btn  ms-2 p-2 text-white fs-4" style="background-color:#A30132; ">Delete</button>
         </div>`
         list.appendChild(note_item)
      
@@ -16,7 +16,22 @@ document.getElementById('btn-add').addEventListener('click',()=>{
     }
     
 })
-document.getElementsByClassName('btn_remove').addEventListener('click',()=>{
-    
-
+document.getElementById('list').addEventListener('click',()=>{
+    if(event.target.classList.contains('btn_remove')){
+        event.target.parentElement.parentElement.remove()
+    }
+  
+})
+document.getElementById('list').addEventListener('change',()=>{
+    if(event.target.type==='checkbox'){
+       let check=event.target.closest('li').querySelector('.note-of-todo')
+         if(event.target.checked){
+              check.style.textDecoration='line-through'
+              check.style.textDecorationColor='#A30132'
+         }
+         else{
+              check.style.textDecoration='none'
+         }
+    }
+  
 })
